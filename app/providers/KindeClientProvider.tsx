1e935e1e0c21f5dd02870c60d2236b5f758d36c1
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-27 09:25:47 -0700

    feat(Providers): added Kinde provider to Project

---
+++ app/providers/KindeClientProvider.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
+
+export function KindeClientProvider({ children }: { children: React.ReactNode }) {
+  return <KindeProvider>{children}</KindeProvider>;
+}
