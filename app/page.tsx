1f87c6afe7d86110f8e0b1088ecf152ec88237a0
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-27 09:26:02 -0700

    feat: demo homepage added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,41 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      hello world
-    </div>
+    <section className="h-screen flex items-center justify-center">
+      <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold tracking-tighter text-gray-900 lg:text-5xl text-balance">
+            Akira
+            <span className="text-gray-600"> Project Manager</span>
+          </h1>
+          <p className="mt-4 text-base font-medium text-gray-500">
+            The fastest method for working together
+            <span className="md:block">
+              {" "}
+              on staging and temporary environments.
+            </span>
+          </p>
+          <div className="flex flex-col items-center justify-center gap-2 mx-auto mt-8 md:flex-row">
+            <Link href="/login">
+              <button
+                className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 bg-gray-100 md:w-auto rounded-xl hover:bg-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                aria-label="Download on Google Play"
+              >
+                Login
+              </button>
+            </Link>
+            <Link href="/register">
+              <button
+                className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 bg-gray-100 md:w-auto rounded-xl hover:bg-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                aria-label="Download on App Store"
+              >
+                Sign up
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- app/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <KindeProvider>{children}</KindeProvider>;
-}
