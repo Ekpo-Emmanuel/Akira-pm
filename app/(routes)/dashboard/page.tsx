f0ffe270ccccdb33ebde213815d3e8ab96c2bcce
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-27 13:04:45 -0700

    refactor: update Dashboard page with user organization data and pending invitations

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import { LoginLink, LogoutLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+'use client';
 
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import PendingInvitations from "./_components/PendingInvitations";
+import { useUser } from "@/app/contexts/UserProvider";
 
-export default function page() {
+export default function Dashboard() {
+  const { user } = useUser();
+  const userOrg = useQuery(api.users.getUserOrganization, { kindeId: String(user?.id) });
+  if (userOrg === undefined) return <div>Loading...</div>;
+
   return (
     <div>
-      <LogoutLink>Logout </LogoutLink>
+      <h1>Dashboard</h1>
+      {userOrg ? (
+        <div>You are part of an organization</div>
+      ) : (
+        <PendingInvitations email={String(user?.email)} userId={String(user?.id)} />
+      )}
     </div>
-  )
+  );
 }
