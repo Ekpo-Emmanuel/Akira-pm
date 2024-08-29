673ec78d81b5314581aab43dab0f6932aedd20e9
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-29 15:10:33 -0700

    refactor: modularize Dashboard component and integrate Welcome and Workspaces components

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -4,20 +4,25 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import PendingInvitations from "./_components/PendingInvitations";
 import { useUser } from "@/app/contexts/UserProvider";
+import Welcome from "./_components/dashboard/Welcome";
+import Workspaces from "./_components/dashboard/Workspaces";
 
 export default function Dashboard() {
   const { user } = useUser();
-  const userOrg = useQuery(api.users.getUserOrganization, { kindeId: String(user?.id) });
-  if (userOrg === undefined) return <div>Loading...</div>;
+  // const userOrg = useQuery(api.users.getUserOrganization, { kindeId: String(user?.id) });
+  // if (userOrg === undefined) return <div>Loading...</div>;
 
   return (
     <div>
-      <h1>Dashboard</h1>
-      {userOrg ? (
+      <div className="grid grid-cols-1 gap-2">
+        <Welcome name={String(user?.given_name)} />
+        <Workspaces />
+      </div>
+      {/* {userOrg ? (
         <div>You are part of an organization</div>
       ) : (
         <PendingInvitations email={String(user?.email)} userId={String(user?.id)} />
-      )}
+      )} */}
     </div>
   );
 }
