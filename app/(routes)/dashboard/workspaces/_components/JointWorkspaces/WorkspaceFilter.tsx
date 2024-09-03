4595d074436f0f4edcf182a2a4cd2ee69ca4e423
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-02 18:41:09 -0700

    refactor: Simplify WorkspaceFilter component and update styles

--- app/(routes)/dashboard/workspaces/_components/JointWorkspaces/WorkspaceFilter.tsx
+++ app/(routes)/dashboard/workspaces/_components/JointWorkspaces/WorkspaceFilter.tsx
@@ -1,25 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
-import {
-  Home,
-  Settings,
-  FileText,
-  PanelsTopLeft,
-  ChevronDown,
-  Plus,
-  Landmark,
-} from "lucide-react";
-import clsx from "clsx";
-
 import {
   Select,
   SelectContent,
@@ -29,16 +10,14 @@ import {
 } from "@/components/ui/select"
 
 
-type Checked = DropdownMenuCheckboxItemProps["checked"];
-
 export default function WorkspaceFilter() {
   return (
     <div>
       <Select>
         <SelectTrigger className="p-0 h-8 w-fit">
-          <SelectValue placeholder="Filter By" />
+          <SelectValue className="placeholder:text-gray-900" placeholder="Filter By" />
         </SelectTrigger>
-        <SelectContent className="dark:bg-[#2A2E35] border dark:border-[#656f7d6d]">
+        <SelectContent className="dark:bg-[#2A2E35] border dark:border-borderDark">
           {sortSelection.map((item) => (
             <SelectItem key={item.name} value={item.name}>
               {item.name}
