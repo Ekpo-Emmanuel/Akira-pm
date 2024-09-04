51e1832e350a2141e528ca782e13f51a24042273
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-04 15:29:58 -0700

    feat: Add UserProviderClient to manage user state across the client-side
    - Create a `UserContext` using React Context API to manage user state globally.
    - Implement `UserProviderClient` to provide the `UserContext` with an initial user state and expose the user and setUser through the context.
    - Develop a custom hook `useUser` to access and manage the user context, enforcing usage within the `UserProviderClient`.
    - Ensure that the context throws a descriptive error when used outside of its provider, enhancing debugging and developer experience.

---
+++ app/contexts/user/UserProviderClient.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React, { createContext, useContext, useState } from 'react';
+import { User } from '@/app/types';
+
+type UserContextType = {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export function UserProviderClient({ children, initialUser }: { children: React.ReactNode; initialUser: User | null }) {
+  const [user, setUser] = useState<User | null>(initialUser);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+}
