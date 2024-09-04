893d1842bd599dde09403c88584fee71202ce326
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-04 15:14:36 -0700

    feat: Enhance OrganizationPrompt with joining functionality
    - Expanded the OrganizationPrompt component to include both creating and joining organization capabilities.
    - Introduced state management for organization ID input and a toggle between creating or joining an organization.
    - Added error handling and success notifications using the `toast` utility to improve user feedback during the organization creation or joining process.
    - Refactored the component to use newly structured hooks from `/dashboard/_utils/organizationUtils` for cleaner and more maintainable code.

--- app/(routes)/dashboard/_components/OrganizationPrompt.tsx
+++ app/(routes)/dashboard/_components/OrganizationPrompt.tsx
@@ -1,70 +1,124 @@
 'use client';
 
 import { useState } from 'react';
-import { useCreateOrganization, useJoinOrganization, useGetUserOrganization } from '@/app/(routes)/dashboard/_utils/organizationUtils';
+import { useCreateOrganization, useJoinOrganization } from '@/app/(routes)/dashboard/_utils/organizationUtils';
 import { Id } from "@/convex/_generated/dataModel";
+import { toast } from 'sonner';
+import { useOrganization } from '@/app/contexts/organization/OrganizationContext';
+import { Organization } from "@/app/types"; 
 
-export default function OrganizationPrompt({ userId }: { userId: string }) {
+type OrganizationPromptProps = {
+  userId: string;
+  onCreateOrganization: (org: Organization) => void;
+};
+
+export default function OrganizationPrompt({ userId, onCreateOrganization }: OrganizationPromptProps) {
+  const { currentOrganization, setCurrentOrganization, userOrganizations, setUserOrganizations, isLoading: isOrgLoading } = useOrganization();
   const [orgName, setOrgName] = useState('');
+  const [orgId, setOrgId] = useState('');
+  const [isJoining, setIsJoining] = useState(false);
   const createOrganization = useCreateOrganization();
   const joinOrganization = useJoinOrganization();
-  const userOrg = useGetUserOrganization(userId);
 
-  if (userOrg === undefined) return <div>Loading...</div>;
-  if (userOrg) return null; // User has a valid organization
+  if (isOrgLoading) {
+    return <div>Loading Organization...</div>;
+  }
 
-  const handleCreateOrg = async () => {
+  if (currentOrganization || userOrganizations.length > 0) {
+    return null;
+  }
+
+  const handleCreateOrganization = async () => {
     if (orgName) {
       try {
-        await createOrganization(orgName, userId);
+        const result = await createOrganization(orgName, userId);
+        if ('orgId' in result && 'workspaceId' in result) {
+          const newOrg: Organization = {
+            id: result.orgId,
+            name: orgName,
+            creatorId: userId
+          };
+          setUserOrganizations([...userOrganizations, newOrg]);
+          onCreateOrganization(newOrg);
+          toast.success('Organization created successfully');
+        }
       } catch (error) {
-        // Handle error (e.g., show error message to user)
+        console.error("Failed to create organization:", error);
       }
     }
   };
 
-  const handleJoinOrg = async () => {
-    const orgIdString = prompt("Enter organization ID:");
-    if (orgIdString) {
+  const handleJoinOrganization = async () => {
+    if (orgId) {
       try {
-        const orgId = orgIdString as Id<"organizations">;
-        await joinOrganization(orgId, userId);
+        const organizationId = orgId as Id<"organizations">;
+        const joinedOrg = await joinOrganization(organizationId, userId);
+        
+        if ('id' in joinedOrg && 'name' in joinedOrg && 'creatorId' in joinedOrg) {
+          setUserOrganizations([...userOrganizations, joinedOrg as Organization]);
+          onCreateOrganization(joinedOrg as Organization);
+        }
+        toast.success('Successfully joined the organization');
       } catch (error) {
-        // Handle error (e.g., show error message to user)
+        console.error("Failed to join organization:", error);
       }
     }
   };
-
 
   return (
-    <section className="flex items-center justify-center w-full h-screen bg-gray-50 dark:bg-gray-900 fixed z-50">
+    <section className="flex items-center justify-center w-full h-screen bg-gray-50 dark:bg-bgDark fixed z-50">
       <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center">
-        <div className="bg-white  dark:bg-gray-800 border dark:border-gray-700 p-6 sm:p-8 rounded-lg shadow-xl">
+        <div className="bg-white dark:bg-workspaceDark border dark:border-borderDark p-6 rounded-lg shadow-xl">
           <h2 className="text-2xl font-bold mb-4 dark:text-white">Welcome! Let's get you started</h2>
           <div className="mb-4">
-            <input
-              type="text"
-              value={orgName}
-              onChange={(e) => setOrgName(e.target.value)}
-              placeholder="Organization name"
-              className="w-full p-2 border rounded placeholder:text-sm"
-            />
+            <div className="flex space-x-2 mb-4">
+              <button
+                onClick={() => setIsJoining(false)}
+                className={`px-4 py-2 h-8 w-full text-sm rounded text-white ${!isJoining ? 'bg-blue-500' : 'bg-[#3C414A] text-gray-700'}`}
+              >
+                Create
+              </button>
+              <button
+                onClick={() => setIsJoining(true)}
+                className={`px-4 py-2 h-8 w-full text-sm rounded text-white ${isJoining ? 'bg-green-500' : 'bg-[#3C414A] text-gray-700'}`}
+              >
+                Join
+              </button>
+            </div>
+            {isJoining ? (
+              <>
+                <label className="text-sm mb-2">Organization Id</label>
+                <input
+                  type="text"
+                  value={orgId}
+                  onChange={(e) => setOrgId(e.target.value)}
+                  className="w-full p-2 border dark:border-borderDark bg-inherit rounded placeholder:text-sm mb-2"
+                />
+                <button
+                  onClick={handleJoinOrganization}
+                  className="bg-green-500 hover:bg-green-600 text-sm text-white px-4 py-2 rounded w-full"
+                >
+                  Join Organization
+                </button>
+              </>
+            ) : (
+              <>
+                <label className="text-sm mb-2">Organization Name</label>
+                <input
+                  type="text"
+                  value={orgName}
+                  onChange={(e) => setOrgName(e.target.value)}
+                  className="w-full p-2 border dark:border-borderDark bg-inherit rounded placeholder:text-sm mb-2"
+                />
+                <button
+                  onClick={handleCreateOrganization}
+                  className="bg-blue-500 hover:bg-blue-600 text-sm text-white px-4 py-2 rounded w-full"
+                >
+                  Create Organization
+                </button>
+              </>
+            )}
           </div>
-          <button
-            onClick={handleCreateOrg}
-            className="bg-blue-500 hover:bg-blue-600 text-sm text-white px-4 py-2 rounded w-full"
-          >
-            Create Organization
-          </button>
-          {/* <button
-            onClick={handleJoinOrg}
-            className="bg-green-500 text-sm text-white px-4 py-2 rounded"
-          >
-            Join Existing Organization
-          </button> */}
-        <div className="mt-4" onClick={handleJoinOrg}>
-          <p className="dark:text-gray-300 text-xs">or Join an existing <span className="text-blue-500 cursor-pointer">organization</span></p>
-        </div>
         </div>
       </div>
     </section>
