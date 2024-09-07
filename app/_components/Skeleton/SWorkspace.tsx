e075199013b1adb1941b114f407b2cc41983f824
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-06 22:00:36 -0700

    fix sheleton component

--- app/_components/Skeleton/SWorkspace.tsx
+++ app/_components/Skeleton/SWorkspace.tsx
@@ -2,11 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function SWorkspace() {
   return (
-    <div className="flex flex-col space-y-3 p-3 md:p-6 relative">
+    <div className="flex flex-col space-y-3 p-3 md:p-6 relative h-full">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin" />
         </div>
-      <Skeleton className="h-[25px] w-full rounded-none" />
+      {/* <Skeleton className="h-[25px] w-full rounded-none" />
       <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
           <Skeleton className="h-[125px] w-full rounded-md" />
           <Skeleton className="h-[125px] w-full rounded-md" />
@@ -14,7 +14,7 @@ export function SWorkspace() {
           <Skeleton className="h-[125px] w-full rounded-md" />
           <Skeleton className="h-[125px] w-full rounded-md" />
           <Skeleton className="h-[125px] w-full rounded-md" />
-      </div>
+      </div> */}
     </div>
   )
 }
