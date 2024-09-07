7a21537331346c702fe82da2c290cbdbfa746d04
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-06 21:56:04 -0700

    refactor: Optimize rendering of workspace details page components
    Update WorkspacePage to streamline component rendering by removing redundant state and useEffect hook for navigation. Maintain essential functionality with a clean and efficient component structure, improving performance and maintainability of the workspace details page.

--- app/(routes)/dashboard/workspaces/[workspaceId]/page.tsx
+++ app/(routes)/dashboard/workspaces/[workspaceId]/page.tsx
@@ -1,6 +1,5 @@
 'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { Id } from "@/convex/_generated/dataModel";
@@ -8,21 +7,12 @@ import SingleWorkspaceHeader from "../_components/Single workspace/SingleWorkspa
 import WorkspaceTabs from "../_components/Single workspace/WorkspaceTabs";
 
 export default function WorkspacePage({ params }: { params: { workspaceId: string } }) {
-  const router = useRouter();
   const workspace = useQuery(api.workspaces.getWorkspace, { id: params.workspaceId as Id<"workspaces"> });
-
-  useEffect(() => {
-    router.replace(`/dashboard/workspaces/${params.workspaceId}/recent`);
-  }, [router, params.workspaceId]);
-
-  if (!workspace) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <section>
       <SingleWorkspaceHeader />
-      <WorkspaceTabs workspaceId={params.workspaceId} />
+      <WorkspaceTabs workspaceId={params.workspaceId as Id<"workspaces">} />
     </section>
   );
 }
