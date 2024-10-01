aeadf48d115f567effbd2d363cc59398e74c4546
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-10-01 16:19:18 -0700

    refactor: update board creation logic with default columns and groups
    - Refactored `createBoard` mutation to include default columns and groups when a new board is created.
    - Columns for the default table include Task, Status (with predefined options), Due Date, and Assignees.
    - Each new board now includes a "Main Table" with an initial "Default Group" for better organization.
    - Updated the structure to include `createdAt` and `updatedAt` timestamps for better tracking.
    - No changes to functionality, focused on improving code structure and clarity.

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,4 +1,5 @@
 import { mutation, query } from "./_generated/server";
+import { Column } from "@/app/types";
 import { v } from "convex/values";
 
 export const createBoard = mutation({
@@ -16,17 +17,37 @@ export const createBoard = mutation({
         createdBy: args.userId,
       });
       
+      const defaultColumns: Column[] = [
+        { id: "task", name: "Task", type: "text" },
+        { 
+          id: "status", 
+          name: "Status", 
+          type: "status",
+          options: [
+            { id: 'notStarted', name: 'Not Started', color: '#797E93' },
+            { id: 'inProgress', name: 'In Progress', color: '#F7BC63' },
+            { id: 'completed', name: 'Completed', color: '#62D491' },
+          ]
+        },
+        { id: "dueDate", name: "Due Date", type: "date" },
+        { id: "assignees", name: "Assignees", type: "people" },
+      ];
+
+
       await ctx.db.insert("tables", {
         boardId,
         name: "Main Table",
-        description: "This is your first table in this board",
-        columns: [
-          { id: "1", name: "Task", type: "text" },
-          { id: "2", name: "Status", type: "select" },
-          { id: "3", name: "Due Date", type: "date" }
+        description: "First table in this board",
+        columns: defaultColumns,
+        groups: [
+          {
+            id: "default",
+            name: "Default Group",
+            rows: [],
+          }
         ],
-        rows: [],
         createdBy: args.userId,
+        createdAt: Date.now(),
         updatedAt: Date.now(),
       });
 
